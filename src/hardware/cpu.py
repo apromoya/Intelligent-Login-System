113f061927b9251350100d0837c1deee918943b7
apromoya <107261249+apromoya@users.noreply.github.com>
2026-05-11 15:56:54 -0500

    feat(cpu): implementacion del ciclo fetch-decode-execute y logica de control

--- src/hardware/cpu.py
+++ src/hardware/cpu.py
@@ -1,4 +1,42 @@
+# src/hardware/cpu.py
+
+from .alu import ALU
+
 class CPU:
-    def fetch_decode_execute(self, datos, memoria):
-        print(f"[CPU]: Procesando datos: {datos}")
-        return "Acceso Denegado (Simulación)"
+    def __init__(self):
+        self.alu = ALU()  # La CPU tiene su propia unidad aritmética
+        self.registro_instruccion = None
+
+    def fetch_decode_execute(self, datos, memoria_ram):
+        """
+        Ciclo de vida de la instrucción de Login.
+        """
+        # 1. FETCH & DECODE (Recibir y entender qué nos enviaron)
+        username = datos.get("user")
+        password_intento = datos.get("pass")
+        self.registro_instruccion = "VALIDAR_ACCESO"
+
+        print(f"[CPU]: Ejecutando {self.registro_instruccion} para el usuario: {username}")
+
+        # 2. ACCESO A MEMORIA (RAM)
+        # Verificamos si el usuario ya está en la RAM y si está bloqueado
+        intentos = memoria_ram.leer_intentos(username)
+        
+        if intentos >= 3:
+            return "ESTADO: BLOQUEADO (Demasiados intentos en RAM)"
+
+        # 3. EXECUTE (Llamamos a la ALU para la comparación lógica)
+        # Aquí la CPU le pasa los datos a la ALU para que ella haga el 'trabajo sucio'
+        # Simulamos que traemos el hash real de la DB (esto lo haremos más adelante)
+        hash_real = "hash_quemado_provisional" # Luego vendrá del archivo/disco
+        
+        # La ALU procesa y activa sus banderas (flags)
+        es_valido = self.alu.comparar(password_intento, hash_real)
+
+        if es_valido:
+            memoria_ram.resetear_intentos(username)
+            return "ESTADO: ACCESO CONCEDIDO"
+        else:
+            # Si falla, la CPU ordena a la RAM aumentar el contador
+            nuevo_conteo = memoria_ram.registrar_fallo(username)
+            return f"ESTADO: ACCESO DENEGADO (Intento {nuevo_conteo}/3)"
--- src/hardware/memory.py
+++ src/hardware/memory.py
@@ -1,3 +1,18 @@
+# src/hardware/memory.py
+
 class RAM:
     def __init__(self):
-        self.intentos_fallidos = {} # Simulación de celdas de memoria
+        # Nuestro banco de memoria volátil
+        self.celdas_intentos = {}
+
+    def leer_intentos(self, usuario):
+        return self.celdas_intentos.get(usuario, 0)
+
+    def registrar_fallo(self, usuario):
+        actual = self.celdas_intentos.get(usuario, 0)
+        self.celdas_intentos[usuario] = actual + 1
+        return self.celdas_intentos[usuario]
+
+    def resetear_intentos(self, usuario):
+        if usuario in self.celdas_intentos:
+            self.celdas_intentos[usuario] = 0
